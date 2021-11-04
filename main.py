1e4269d9dee7d23011ceb4c8336d9c630ca3a6c6
Gustavo Tomás <guga.tomas10@gmail.com>
2021-11-04 09:11:59 -0300

    added reply on #

--- main.py
+++ main.py
@@ -26,23 +26,25 @@ def set_last_seen_id(last_seen_id, file):
   f_write.close()
   return
 
+def get_days_till_krismas(tweet):
+  today = tweet.created_at.date()
+  krismas = datetime.date(today.year, 12, 25)
+  return str((krismas - today).days)
+
 def reply_to_krismas():
   print('Looking for krismas...')
   last_seen_id = get_last_seen_id(LAST_SEEN)
-  mentions = api.mentions_timeline(since_id=last_seen_id)
-
-  for mention in reversed(mentions):
-    today = mention.created_at.date()
-    krismas = datetime.date(today.year, 12, 25)
-    days_left = krismas - today
-    print(str(mention.id) + ' - ' + mention.text + ' - ' + str(today))
-    
-    last_seen_id = mention.id
-    set_last_seen_id(last_seen_id, LAST_SEEN)
-    if '#krismas' in mention.text.lower():
-      print('responding to ' + mention.user.screen_name)
-      status = '@' + mention.user.screen_name + ' ' + str(days_left.days) + ' days till krismas \U0001F563'
-      api.update_status(status, in_reply_to_status_id=mention.id)
+  tweets = api.search_tweets(q='#krismas', since_id=last_seen_id)
+  for tweet in tweets:
+    hashtags = tweet.entities.get('hashtags')
+    print(str(tweet.id) + ' - ' + tweet.user.screen_name)
+    if any(hashtag.get('text') == 'krismas' for hashtag in hashtags):
+      days_left = get_days_till_krismas(tweet)
+      last_seen_id = tweet.id
+      set_last_seen_id(last_seen_id, LAST_SEEN)
+      status = '@' + tweet.user.screen_name + ' ' + days_left + ' days till krismas \U0001F563'
+      api.update_status(status, in_reply_to_status_id=tweet.id)
+      print('responded to ' + tweet.user.screen_name)
 
 while True:
   reply_to_krismas()
